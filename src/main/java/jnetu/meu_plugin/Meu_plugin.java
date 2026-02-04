2f54e2d7b5f8993c5ebf44bc7b7a2f19954fd3f2
jnetu <j.neto.rodrigues@ufpi.edu.br>
2026-02-04 17:28:39 -0300

    menu habilidades & home

--- src/main/java/jnetu/meu_plugin/Meu_plugin.java
+++ src/main/java/jnetu/meu_plugin/Meu_plugin.java
@@ -1,22 +1,48 @@
 package jnetu.meu_plugin;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.UUID;
 
 public final class Meu_plugin extends JavaPlugin implements Listener {
+
+    private final HashSet<UUID> modoGeloAtivo = new HashSet<>();
+    private final String TITULO_GUI = "Menu de Habilidades";
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("Plugin de jnetu iniciado com sucesso!");
+        getLogger().info(
+                "\n*****************************************\n" +
+                        "*****************************************\n" +
+                        "Plugin de jnetu iniciado com sucesso!\n" +
+                        "*****************************************\n" +
+                        "*****************************************\n");
+
+        Objects.requireNonNull(getCommand("sethome")).setExecutor(this);
+        Objects.requireNonNull(getCommand("home")).setExecutor(this);
+        Objects.requireNonNull(getCommand("habilidades")).setExecutor(this);
     }
 
     public void onDisable() {
@@ -25,6 +51,13 @@ public final class Meu_plugin extends JavaPlugin implements Listener {
     @EventHandler
     public void aoEntrar(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+
+        //HABILIDADES
+        UUID uuid = player.getUniqueId();
+        if (getConfig().getBoolean("habilidades." + uuid + ".passo-gelado")) {
+            modoGeloAtivo.add(uuid); // Reativa a habilidade na variável
+        }
+
         //mensagem só para o player
         player.sendMessage(Component.text("Bem-vindo ao servidor.", NamedTextColor.GREEN));
         //mensagem global
@@ -33,5 +66,172 @@ public final class Meu_plugin extends JavaPlugin implements Listener {
                 .append(Component.text(" acabou de entrar!", NamedTextColor.GRAY));
 
         Bukkit.broadcast(mensagemGlobal);
+    }
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        // Verificar se quem digitou foi um jogador (console não tem "home")
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Apenas jogadores podem usar comandos");
+            return true;
+        }
+
+        // COMANDO /SETHOME
+        if (command.getName().equalsIgnoreCase("sethome")) {
+            Location loc = player.getLocation();
+            String uuid = player.getUniqueId().toString();
+
+            // Salva config.yml
+            getConfig().set("homes." + uuid + ".world", loc.getWorld().getName());
+            getConfig().set("homes." + uuid + ".x", loc.getX());
+            getConfig().set("homes." + uuid + ".y", loc.getY());
+            getConfig().set("homes." + uuid + ".z", loc.getZ());
+            getConfig().set("homes." + uuid + ".yaw", loc.getYaw());
+            getConfig().set("homes." + uuid + ".pitch", loc.getPitch());
+
+            saveConfig(); // end Salva
+
+            player.sendMessage(Component.text("Home definida com sucesso!", NamedTextColor.GREEN));
+            return true;
+        }
+
+        // COMANDO /HOME
+        if (command.getName().equalsIgnoreCase("home")) {
+            String uuid = player.getUniqueId().toString();
+
+            if (!getConfig().contains("homes." + uuid)) {
+                player.sendMessage(Component.text("Você não tem uma home definida! Use /sethome.", NamedTextColor.RED));
+                return true;
+            }
+
+            // Recupera os dados do arquivo
+            String worldName = getConfig().getString("homes." + uuid + ".world");
+            double x = getConfig().getDouble("homes." + uuid + ".x");
+            double y = getConfig().getDouble("homes." + uuid + ".y");
+            double z = getConfig().getDouble("homes." + uuid + ".z");
+            float yaw = (float) getConfig().getDouble("homes." + uuid + ".yaw");
+            float pitch = (float) getConfig().getDouble("homes." + uuid + ".pitch");
+
+            Location homeLoc = new Location(getServer().getWorld(worldName), x, y, z, yaw, pitch);
+
+            player.teleport(homeLoc);
+            player.sendMessage(Component.text("Teleportado para sua home!", NamedTextColor.AQUA));
+            return true;
+        }
+        //end /HOME
+
+        //HABILIDADES GUI
+        if (command.getName().equalsIgnoreCase("habilidades")) {
+                abrirMenuHabilidades(player);
+            return true;
+        }
+
+
+        return false;
+    }
+
+    private void abrirMenuHabilidades(Player player) {
+
+        Inventory gui = Bukkit.createInventory(null, 27, Component.text(TITULO_GUI));
+
+        // Cria o item que representa a habilidade
+        ItemStack itemGelo = new ItemStack(Material.ICE);
+        ItemMeta meta = itemGelo.getItemMeta();
+
+        meta.displayName(Component.text("Habilidade: Passo Gelado", NamedTextColor.AQUA));
+
+        // Altera a descrição com base no estado atual
+        boolean estaAtivo = modoGeloAtivo.contains(player.getUniqueId());
+        meta.lore(java.util.List.of(
+                Component.text("Status: " +
+                                (estaAtivo ? "ATIVADO" : "DESATIVADO"),
+                        estaAtivo ? NamedTextColor.GREEN : NamedTextColor.RED)
+        ));
+
+        itemGelo.setItemMeta(meta);
+        gui.setItem(13, itemGelo);
+
+        player.openInventory(gui);
+    }
+
+    @EventHandler
+    public void aoClicarNoMenu(InventoryClickEvent event) {
+        // Verificar se é menu
+        if (!event.getView().title().equals(Component.text(TITULO_GUI))) return;
+
+        event.setCancelled(true); // Impede o player de pegar o item do menu
+
+        if (event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR) return;
+
+        Player player = (Player) event.getWhoClicked();
+        UUID uuid = player.getUniqueId();
+
+        // Alterna o estado da habilidade
+        if (modoGeloAtivo.contains(uuid)) {
+            modoGeloAtivo.remove(uuid);
+            salvarEstadoGelo(uuid, false);
+
+            player.sendMessage(Component.text("Habilidade de Gelo", NamedTextColor.WHITE)
+                    .append(Component.text(" DESATIVADA!", NamedTextColor.RED)));
+        } else {
+            modoGeloAtivo.add(uuid);
+            salvarEstadoGelo(uuid, true);
+            player.sendMessage(Component.text("Habilidade de Gelo", NamedTextColor.WHITE)
+                    .append(Component.text(" ATIVADA!", NamedTextColor.GREEN)));
+
+            ;
+        }
+
+        player.closeInventory(); // Fecha o menu
+    }
+    private boolean modoGeloPlayer(UUID uuid){
+        return modoGeloAtivo.contains(uuid);
+    }
+
+    private void salvarEstadoGelo(UUID uuid, boolean ativo) {
+        getConfig().set("habilidades." + uuid + ".passo-gelado", ativo);
+        saveConfig();
+    }
+/* ----old movement ----
+    @EventHandler
+    public void movement(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (!modoGeloAtivo.contains(player.getUniqueId())) return;
+        Location loc = event.getPlayer().getLocation();
+        loc.setY(loc.getY() - 1);
+        Block b = loc.getBlock();
+        Material material = b.getType();
+        switch (material) {
+            case WATER:
+                b.setType(Material.ICE);
+                break;
+        }
+    }
+
+ */
+    @EventHandler
+    public void movement(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (!modoGeloAtivo.contains(player.getUniqueId())) return;
+
+        // --- CONFIGURAÇÃO ---
+        int raio = 0;
+        //Location loc = player.getLocation(); ->lagless server
+        Location loc = event.getTo();
+        if (loc == null) return;
+        for (int x = -raio; x <= raio; x++) {
+            for (int z = -raio; z <= raio; z++) {
+                Block block = loc.clone().add(x, -1, z).getBlock();
+                if (block.getType() == Material.WATER) {
+                    if (block.getBlockData() instanceof org.bukkit.block.data.Levelled levelled) {
+                        if (levelled.getLevel() == 0) {
+                            if (block.getRelative(0, 1, 0).getType() == Material.AIR) {
+                                block.setType(Material.ICE);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
